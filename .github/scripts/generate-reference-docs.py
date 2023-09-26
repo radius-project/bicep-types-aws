3d88dc307fc4f175f635c64b8f5b3cd70afc08db
Will Smith <willdavsmith@gmail.com>
2023-09-26 10:33:52 +0100

    Add GitHub workflow for downloading and generating AWS types (#22)
    * Adding GitHub workflow for downloading and generating AWS types

---
+++ .github/scripts/generate-reference-docs.py
@@ -0,0 +1,69 @@
+# ------------------------------------------------------------
+# Copyright 2023 The Radius Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------
+
+import os
+import sys
+import json
+
+
+def generate_reference_table(base_directory, output_filename):
+    with open(output_filename, "w") as output_file:
+        output_file.write("| Resource Type | Primary Identifier(s) | Notes |\n")
+        output_file.write("| ------------- | --------------------- | ----- |\n")
+
+        filepaths = os.listdir(base_directory)
+        filepaths.sort()
+
+        for filepath in filepaths:
+            with open(f"{base_directory}/{filepath}", "r") as input_file:
+                data = json.load(input_file)
+                resource_type = data["type"]
+                resource_schema = data["schema"]
+                primary_identifiers = resource_schema["primaryIdentifier"]
+                is_nonidempotent = False
+                for primary_identifier in primary_identifiers:
+                    if "readOnlyProperties" in resource_schema:
+                        property_list = resource_schema["readOnlyProperties"]
+                        if primary_identifier in property_list:
+                            is_nonidempotent = True
+
+                primary_identifiers_output = ", ".join(primary_identifiers)
+                notes_output = ""
+                if is_nonidempotent:
+                    notes_output = "\u26a0 This resource type is non-idempotent. See [here](https://github.com/radius-project/bicep-types-aws/blob/main/docs/reference/supported_resources.md) for more information."
+                
+                aws_resource_type = resource_type.split("::")[1].lower()
+                resource_api_version = "default"
+                resource_link = f"https://github.com/radius-project/bicep-types-aws/blob/main/artifacts/bicep/aws/aws.{aws_resource_type}/{resource_api_version}/types.md"
+
+                output_file.write(
+                    f"| **[{resource_type}]({resource_link})** | {primary_identifiers_output} | {notes_output} |\n"
+                )
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: generate_reference_docs.py <base_directory> <output_filename>")
+        sys.exit(1)
+
+    base_directory = sys.argv[1]
+    output_filename = sys.argv[2]
+
+    generate_reference_table(base_directory, output_filename)
+
+
+if __name__ == "__main__":
+    main()
---
+++ docs/reference/limitations.md
@@ -0,0 +1,4 @@
+## Radius Bicep AWS limitations
+
+### non-idempotency
+If you see the a warning describing an AWS resource as 'non-idempotent', this means that this resource type is assigned a primary identifier at deployment time. It is currently not supported by Radius Bicep.
